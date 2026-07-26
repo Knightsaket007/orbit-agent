2e527861d55992523fad249f39e32d1403f11017
Knightsaket007 <saketsourav3@gmail.com>
2026-07-26 23:47:58 +0530

    clean main file of frontend

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,24 +3,3 @@ import { App } from "./App";
 import { executeTask } from "./services/task.service";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = App();
-
-
-
-const button = document.querySelector("#execute-btn") as HTMLButtonElement;
-button.addEventListener("click", async () => {
-
-    const textarea = document.querySelector("#prompt") as HTMLTextAreaElement;
-
-    const prompt = textarea.value.trim();
-
-    if (!prompt) return;
-
-    console.log(prompt);
-
-    const result = await executeTask({
-        prompt,
-    });
-
-    console.log(result);
-
-});
