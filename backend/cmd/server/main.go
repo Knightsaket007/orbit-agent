393702a4495c0bce2b934e16248bd7a2b616da75
Knightsaket007 <saketsourav3@gmail.com>
2026-07-26 23:46:38 +0530

    cor added for browser based http connection

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -2,6 +2,7 @@ package main;
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 	"github.com/Knightsaket007/orbit-agent/backend/internal/routes"
 )
 
@@ -13,6 +14,7 @@ func main() {
 		gin.Logger(),
 		gin.Recovery(),
 	)
+	router.Use(cors.Default())
 
 	routes.Register(router)
 
